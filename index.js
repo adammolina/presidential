571de29cb16f38800e04d381909d960b1c1b670c
Lukas Mo <67667042+adammolina@users.noreply.github.com>
2020-09-26 18:31:53 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,2 @@
-var images = document.querySelectorAll(".pictureButtons").length;
-
-for (var i = 0; i<images; i++) {
-
-  document.querySelectorAll(".pictureButtons")[i].addEventListener("click", function () {
-
-    buttonAnimation()
-
-  });
-}
-
-
-function buttonAnimation(currentKey) {
-
-  var activeButton = document.querySelector("." + currentKey);
-  activeButton.classList.add("imgClicked");
-}
+$()
+img
